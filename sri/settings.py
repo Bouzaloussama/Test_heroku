df81290215314dfcb2fb315021d5a7b509258514
bouzal <49028204+Bouzaloussama@users.noreply.github.com>
2021-03-31 18:37:28 +0200

    Update settings.py

--- sri/settings.py
+++ sri/settings.py
@@ -15,6 +15,18 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+#=====================================
+
+# solvr keroku
+# Activate Django-Heroku.
+#django_heroku.settings(locals())
+#BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_ROOT = os.path.join(BASE_DIR, )#'staticfiles')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+#=====================================
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
