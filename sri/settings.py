37f6b0563738f6bd7bd9ffadb5d6a592d8397d8c
bouzal <49028204+Bouzaloussama@users.noreply.github.com>
2021-03-31 23:57:55 +0200

    Update settings.py

--- sri/settings.py
+++ sri/settings.py
@@ -128,8 +128,9 @@ USE_TZ = True
 # solvr keroku
 # Activate Django-Heroku.
 #django_heroku.settings(locals())
-STATIC_ROOT = os.path.join(BASE_DIR, 'static) #files')
-#STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+              os.path.join(BASE_DIR, 'static') ]
 
 #=====================================
 
