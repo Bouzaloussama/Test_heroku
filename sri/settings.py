18116a1f938ae9d0f5ac10103403e74ff3659416
bouzal <49028204+Bouzaloussama@users.noreply.github.com>
2021-03-31 19:20:39 +0200

    Update settings.py

--- sri/settings.py
+++ sri/settings.py
@@ -130,7 +130,6 @@ django_heroku.settings(locals())
 #django_heroku.settings(locals())
 #BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 #STATIC_ROOT = os.path.join(BASE_DIR, )#'staticfiles')
-STATIC_URL = '/static/'
 #STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 #=====================================
